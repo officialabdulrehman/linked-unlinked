0cf0953133b3a30529060d63e5239a0eb8bf66c9
officialabdulrehman <officialabdulrehman@gmail.com>
2021-01-01 17:28:06 +0500

    Done

--- linked.js
+++ linked.js
@@ -1,1 +1,11 @@
 // find all the users that have not been linked
+
+import { linked, allUsers } from "./users";
+
+function compareAndContrast(linked, allUsers){
+  return unlinked = allUsers.filter(function(obj1){
+    return !linked.some(function(obj2){
+      return obj1.id === obj2.id
+    })
+  })
+}
